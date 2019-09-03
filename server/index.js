d503cc30a3f59f4c136ffceabaec48d860dd0a6e
oh2471 <oshpg@naver.com>
2019-09-03 09:38:44 +0900

    Create README.md
    wallet 생성방법

---
+++ server/index.js
@@ -0,0 +1,5 @@
+var Web3 = require("web3")
+web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    
+var newAccount = web3.eth.personal.newAccount("yo");
+console.log(newAccount)
