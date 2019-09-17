7757504ee049c334cad5e29856f3d80733dac118
Jimyeol <controlexp@naver.com>
2019-09-17 14:47:53 +0900

    solidity updates

--- solidity/purchase_test.sol
+++ solidity/purchase_test.sol
@@ -1,7 +1,5 @@
 pragma solidity ^0.5.0;
 
-//Slight Modification to run in Remix
-//Source: http://solidity.readthedocs.io/en/v0.3.2/solidity-by-example.html#safe-remote-purchase
 
 contract purtest {
     
@@ -9,6 +7,8 @@ contract purtest {
     
     struct Product {
         string name;
+        uint price;
+        address payable owner;
     }
 
     struct Seller {
@@ -23,6 +23,7 @@ contract purtest {
     struct Payment {
         uint paymentId;
         uint price;
+        uint deliveryTime;
         address payable seller_payment;
         address payable buyer_payment;
         State state;
@@ -36,43 +37,52 @@ contract purtest {
     uint price = 0;
     address payable seller;
     uint public seq = 0;
-    uint public producSeq = 0;
+    uint public productSeq = 0;
+    
+    uint private officialDelveryTime = 30;
     
     constructor() public payable{
         seller = msg.sender;
     }
     
     
-    function _purchase_product() public payable {
-        require(msg.value == msg.value);
-        buyerlist[msg.sender].paymentArray.push(seq);
+    function _purchase_product(uint _productId) public payable {
+        require(productlist[_productId].price == msg.value);
         buyerlist[msg.sender].paymentArray.push(seq);
         paylist[seq].paymentId = seq;
         paylist[seq].price = msg.value;
-        paylist[seq].seller_payment = seller;
+        paylist[seq].seller_payment = productlist[_productId].owner;
         paylist[seq].buyer_payment = msg.sender;
         paylist[seq].state = State.Purchase;
+        paylist[seq].deliveryTime = now + officialDelveryTime;
         seq++;
     }
     
-    function _confirm(uint _seq) public payable {
+    function _confirm(uint _seq, uint _productId) public {
         require(msg.sender == paylist[_seq].buyer_payment);
-        require(seller == paylist[_seq].seller_payment);
+        require(productlist[_productId].owner == paylist[_seq].seller_payment);
         require(paylist[_seq].state == State.Purchase);
         seller.transfer(paylist[_seq].price);
         paylist[_seq].state = State.Finish;
     }
     
-    function _adobt(uint _seq) public payable {
+    function _adobt(uint _seq) public {
         require(msg.sender == paylist[_seq].buyer_payment);
         require(paylist[_seq].state == State.Purchase);
         paylist[_seq].buyer_payment.transfer(paylist[_seq].price);
         paylist[_seq].state = State.Finish;
     }
     
-    function set(uint _value) public {
-        sellerlist[msg.sender].productlist[producSeq].
-        price = _value;
-        producSeq++;
+    function _auto_confirm(uint _seq, uint _productId) public {
+        require(now >= paylist[_seq].deliveryTime);
+        _confirm(_seq, _productId);
+    }
+    
+    function set(uint _value, string memory _name) public {
+        sellerlist[msg.sender].products.push(productSeq);
+        productlist[productSeq].name = _name;
+        productlist[productSeq].price = _value;
+        //price = _value;
+        productSeq++;
     }
 }
